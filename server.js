aeb070fc47c700f3a82e3cc2757cf5f4c90b85dd
Dimitri C <couwenberg@gmail.com>
2018-09-08 19:54:40 +1000

    cors test/solution with proxy

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const filebuffer = fs.readFileSync("db/usda-nnd.sqlite3");
 const db = new sqlite.Database(filebuffer);
 
 const app = express();
+const fetch = require('node-fetch');
 
 app.set("port", process.env.PORT || 3001);
 
@@ -25,45 +26,52 @@ const COLUMNS = [
   "description"
 ];
 app.get("/api/food", (req, res) => {
-  const param = req.query.q;
 
-  if (!param) {
-    res.json({
-      error: "Missing required parameter `q`"
-    });
-    return;
-  }
+  fetch('http://api.openweathermap.org/data/2.5/weather?lat=-3.8197&lon=11.0067&appid=c7b5b62a01a84a2d274930a57e180950')
+  .then(res => res.json())
+  .then(json => console.log(json));
 
-  // WARNING: Not for production use! The following statement
-  // is not protected against SQL injections.
-  const r = db.exec(
-    `
-    select ${COLUMNS.join(", ")} from entries
-    where description like '%${param}%'
-    limit 100
-  `
-  );
 
-  if (r[0]) {
-    res.json(
-      r[0].values.map(entry => {
-        const e = {};
-        COLUMNS.forEach((c, idx) => {
-          // combine fat columns
-          if (c.match(/^fa_/)) {
-            e.fat_g = e.fat_g || 0.0;
-            e.fat_g = (parseFloat(e.fat_g, 10) +
-              parseFloat(entry[idx], 10)).toFixed(2);
-          } else {
-            e[c] = entry[idx];
-          }
-        });
-        return e;
-      })
-    );
-  } else {
-    res.json([]);
-  }
+
+  // const param = req.query.q;
+
+  // if (!param) {
+  //   res.json({
+  //     error: "Missing required parameter `q`"
+  //   });
+  //   return;
+  // }
+
+  // // WARNING: Not for production use! The following statement
+  // // is not protected against SQL injections.
+  // const r = db.exec(
+  //   `
+  //   select ${COLUMNS.join(", ")} from entries
+  //   where description like '%${param}%'
+  //   limit 100
+  // `
+  // );
+
+  // if (r[0]) {
+  //   res.json(
+  //     r[0].values.map(entry => {
+  //       const e = {};
+  //       COLUMNS.forEach((c, idx) => {
+  //         // combine fat columns
+  //         if (c.match(/^fa_/)) {
+  //           e.fat_g = e.fat_g || 0.0;
+  //           e.fat_g = (parseFloat(e.fat_g, 10) +
+  //             parseFloat(entry[idx], 10)).toFixed(2);
+  //         } else {
+  //           e[c] = entry[idx];
+  //         }
+  //       });
+  //       return e;
+  //     })
+  //   );
+  // } else {
+  //   res.json([]);
+  // }
 });
 
 app.listen(app.get("port"), () => {
